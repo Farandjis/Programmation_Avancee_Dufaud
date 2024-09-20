e805654eb377d28749ca23b8f29db7e6dcc9adf2
Matthieu Farandjis <22200053@me.uvsq.fr>
2024-09-20 16:00:55 +0200

    fenêtre créer, il y a bien un carré noir qui apparaît

--- TP1/TP1_Mobile_src/UneFenetre.java
+++ TP1/TP1_Mobile_src/UneFenetre.java
@@ -12,6 +12,20 @@ class UneFenetre extends JFrame
 	// ajouter sonMobile a la fenetre
 	// creer une thread laThread avec sonMobile
 	// afficher la fenetre
-	// lancer laThread 
+	// lancer laThread
+
+        JFrame frame = new JFrame("Simple GUI");
+        frame.setPreferredSize(new Dimension(LARG, HAUT));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Display the window
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+
+        UnMobile sonMobile = new UnMobile(LARG, HAUT);
+        sonMobile.setVisible(true);
+
+        frame.add(sonMobile);
     }
 }
---
+++ TP1/rapport.md
@@ -0,0 +1,11 @@
+Matthieu FARANDJIS
+INF.3-FI
+
+<div align="center">
+<img height="95" width="400" src="https://www.uvsq.fr/medias/photo/iut-velizy-villacoublay-logo-2020-ecran_1580904185110-jpg?ID_FICHE=214049" title="logo uvsq vélizy"/>
+
+# Programmation Avancée - Rapport
+
+</div>
+<hr>
+
