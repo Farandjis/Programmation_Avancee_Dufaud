6eba4ee6e55a95ccc9b7f892d811b755b022110e
Matthieu Farandjis <22200053@me.uvsq.fr>
2024-09-20 16:30:42 +0200

    ajout du code du cours pour faire bouger le carré

--- TP1/TP1_Mobile_src/UneFenetre.java
+++ TP1/TP1_Mobile_src/UneFenetre.java
@@ -13,9 +13,10 @@ class UneFenetre extends JFrame
 	// creer une thread laThread avec sonMobile
 	// afficher la fenetre
 	// lancer laThread
-
+        
 
         // Code réutilisé de https://www.thoughtco.com/create-a-simple-window-using-jframe-2034069
+
         JFrame frame = new JFrame("Simple GUI");
         frame.setPreferredSize(new Dimension(LARG, HAUT));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,8 +26,10 @@ class UneFenetre extends JFrame
         frame.pack();
         frame.setVisible(true);
 
+        // 3 lignes venant du cours de M. DUFAUD
         UnMobile sonMobile = new UnMobile(LARG, HAUT);
-        sonMobile.setVisible(true);
+        Thread SupportTache = new Thread(sonMobile);
+        SupportTache.start();
 
         frame.add(sonMobile);
     }
