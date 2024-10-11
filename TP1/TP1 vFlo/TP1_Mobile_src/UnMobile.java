0149c1ec4521dae887eae561e3c63674ae4a6f9f
Matthieu FARANDJIS <azerty@azerty.azerty>
2024-10-11 14:02:35 +0200

    ajout du tp de Florent, je me baserais là dessus pour la suite. En revanche, mon rapport est le mien.

---
+++ TP1/TP1 vFlo/TP1_Mobile_src/UnMobile.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import javax.swing.*;
+
+class UnMobile extends JPanel implements Runnable {
+	int saLargeur, saHauteur, sonDebDessin;
+	final int sonPas = 10, sonTemps = (int) (Math.random()*20)+20, sonCote = 40;
+	private static boolean paused = false;
+
+	UnMobile(int telleLargeur, int telleHauteur) {
+		super();
+		saLargeur = telleLargeur;
+		saHauteur = telleHauteur;
+		setSize(telleLargeur, telleHauteur);
+	}
+
+	public void run() {
+		while (true) {
+			Thread th = new Thread(this);
+			for (sonDebDessin = 0; sonDebDessin + sonCote < saLargeur - sonPas; sonDebDessin += sonPas) {
+				if (paused){
+					th.suspend();
+				}
+
+				repaint();
+				try {
+					Thread.sleep(sonTemps);
+				} catch (InterruptedException telleExcp) {
+					telleExcp.printStackTrace();
+				}
+			}
+
+			for (sonDebDessin = saLargeur - sonCote; sonDebDessin > 0; sonDebDessin -= sonPas) {
+				if (paused){
+					th.suspend();
+				}
+
+				repaint();
+				try {
+					Thread.sleep(sonTemps);
+				} catch (InterruptedException telleExcp) {
+					telleExcp.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void suspend() {
+		paused = true;
+	}
+
+
+	public void paintComponent(Graphics telCG) {
+		super.paintComponent(telCG);
+		telCG.fillRect(sonDebDessin, saHauteur / 2, sonCote, sonCote);
+	}
+}
---
+++ TP1/TP1 vFlo/TP1_Mobile_src/UneFenetre.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+class UneFenetre extends JFrame {
+    UnMobile mobile;
+    private final int LARG = 1000, HAUT = 800;
+    public UneFenetre(int nombre) {
+        JFrame frame = new JFrame("UneFenetre");
+        frame.setSize(LARG, HAUT);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Pause");
+        frame.add(button, BorderLayout.SOUTH);
+
+        Container leConteneur = getContentPane();
+        leConteneur.setLayout (new GridLayout(nombre,1));
+        for (int i=0; i<nombre; i++) {
+            mobile = new UnMobile(LARG, HAUT/nombre);
+            leConteneur.add(mobile);
+
+            Thread thread = new Thread(mobile);
+            thread.start();
+        }
+        frame.add(leConteneur);
+
+
+        button.addActionListener(e -> {
+            if (button.getText().equals("Pause")) {
+                button.setText("Resume");
+                mobile.suspend();
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
